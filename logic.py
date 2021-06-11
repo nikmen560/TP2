5741fe5ad57df749f2c53eb6389fa932097dc00c
Nikita <Kent720p@gmail.com>
2021-06-11 21:10:00 +0300

    created logic and string files

---
+++ logic.py
@@ -0,0 +1,9 @@
+from strings import *
+
+print(mainMenu)
+userInput = int(input("select what do you want "))
+if userInput == 1:
+	print("list of customers")
+	print()
+elif userInput == 2:
+	print("")
